f0e19ea070ebdbd905a17f3ebc64a4699abbb2bb
alessio presciuttini <presciuttinia@gmail.com>
2023-03-09 15:28:20 +0100

    PushDefinitivo

---
+++ LeClassi/Person.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeClassi
+{
+    internal class Person
+    {
+        static int counter = 0;
+        string _name;
+        string _surname;
+        int _age;
+        bool _isAdult;
+        int _bonus;
+        decimal _pilComune;
+        int _maturita;
+        int _universita;
+        bool _fedinaPenale;
+        int _figli;
+        bool _militare;
+        bool _debiti;
+        int _punteggio;
+        int _premio;
+        const int indeceBonus = 35;
+
+        public string Name { get { return _name; } }
+        public string Surname { get { return _surname; } }
+        public string FullName { get { return _name + " " + _surname; } }
+        public bool IsAdult { get { return _isAdult; } }
+        public int Premio { get { return _premio; } }
+
+        public Person(
+
+            string Name,
+            string Surname,
+            int Age,
+            int Maturita,
+            int Universita,
+            bool FedinaPenale,
+            int Figli,
+            bool Militare,
+            bool Debiti,
+            decimal PilComune
+            ) // firma del costruttore
+        {
+            _name = Name;
+            _surname = Surname;
+
+
+            // variabili per il BONUS 
+            _age = Age;
+            _maturita = Maturita;
+            _universita = Universita;
+            _fedinaPenale = FedinaPenale;
+            _figli = Figli;
+            _militare = Militare;
+            _debiti = Debiti;
+            _pilComune = PilComune;
+
+
+            counter++;
+            setIsAdult();
+            if (IsAdult)
+            {
+                CalcolaBonus();
+                _premio = _bonus;
+            }
+
+        }
+        public void getValues()
+        {
+            //Console.WriteLine(
+            //$"Nome:{_name}\n " +
+            //$"Cognome:{_surname}\n" +
+            //$"Age:{_age}" +
+            //$"Maturita:{_maturita}" +
+            //$"FedinaPenale:{_fedinaPenale}" +
+            //$"Debiti: {_debiti}"
+            //);
+
+            Console.WriteLine($"Age:{_age}");
+            Console.WriteLine($"Maturita:{_maturita}");
+            Console.WriteLine($"Debiti:{_debiti}");
+            Console.WriteLine("FedinaPenale:{0}", _fedinaPenale);
+            Console.WriteLine($"Universita:{_universita}");
+            Console.WriteLine($"FedinaPenale:{_fedinaPenale}");
+            Console.WriteLine($"Figli:{_figli}");
+            Console.WriteLine($"Militare:{_militare}");
+            Console.WriteLine($"PilComune:{_pilComune}");
+        }
+        public int getCounter()
+        {
+            return counter;
+        }
+
+        private void setIsAdult()
+        {
+            if (_age > 18)
+            {
+                _isAdult = true;
+            }
+            else
+            {
+                _isAdult = false;
+            }
+        }
+
+        private void CalcolaBonus()
+        {
+
+
+            /*
+               Calcolo per il bonus cittadino
+            */
+
+
+            if (_maturita >= 90)
+            {
+                _punteggio += 7;
+            }
+            if (_age >= 18 && _age <= 28)
+            {
+                _punteggio += 6;
+            }
+            if (_universita >= 28)
+            {
+                _punteggio += 6;
+            }
+            if (!_fedinaPenale)
+            {
+                _punteggio += 4;
+            }
+            switch (_figli)
+            {
+                case 3:
+                    _punteggio += 4;
+                    break;
+                case 4:
+                    _punteggio += 6;
+                    break;
+                case 5:
+                    _punteggio += 8;
+                    break;
+                default:
+                    _punteggio += 2;
+                    break;
+            }
+            if (_militare)
+            {
+                _punteggio += 4;
+            }
+            if (_debiti)
+            {
+                _punteggio += 8;
+            }
+            if (_pilComune < 1000000M)
+            {
+                _punteggio += 8;
+            }
+
+            if (_punteggio >= indeceBonus && _isAdult)
+            {
+                _bonus += 1000;
+            }
+            else
+            {
+                _bonus = 0;
+            }
+        }
+    }
+}
--- LeClassi/Program.cs
+++ LeClassi/Program.cs
@@ -6,7 +6,95 @@ namespace LeClassi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Salve benvenuto nel programma per il calcolo del bonus");
+            Console.WriteLine("Inserisci i tuoi dati per sapere se ne hai diritto");
+            Console.WriteLine("inserisci Nome:");
+            string _name = null;
+            try
+            {
+                _name = Console.ReadLine();
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine("errore" + a);
+            }
+
+            Console.WriteLine("inserisci Cognome:");
+            string _surname = Console.ReadLine();
+
+            Console.WriteLine("Inserisci età:");
+            int _age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserisci voto maturità:");
+            int _voto_maturità = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hai fatto l'università? ,rispondi true o false");
+            bool _università = bool.Parse(Console.ReadLine());
+            int _voto_università = 0;
+            if (_università)
+            {
+                Console.WriteLine("Inserisci voto laurea:");
+                _voto_università = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Hai mai avuto problemi con la legge? rispondi true o false");
+            bool _fedina_penale = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero figli:");
+            int _figli = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hai mai fatto il militare? rispondi true o false");
+            bool _militare = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hai debiti? rispondi true o false");
+            bool _debiti = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserisci Pil comunale:");
+            decimal _pil = decimal.Parse(Console.ReadLine());
+
+            Person person1 = new Person(
+                _name,
+                _surname,
+                _age,
+                _voto_maturità,
+                _voto_università,
+                _fedina_penale,
+                _figli,
+                _militare,
+                _debiti,
+                _pil
+                );
+
+            person1.getValues();
+
+            Console.WriteLine("Stiamo controllando se hai diritto al Bonus....");
+            if (person1.IsAdult)
+            {
+                Console.WriteLine("Essendo maggiorenne ti verrà dato un punteggio");
+
+                if (person1.Premio >= 1000)
+                {
+                    Console.WriteLine("Essendo il tuo punteggio conforme a quanto richiesto riceverai un bonus di 1000€");
+                }
+                else
+                {
+                    Console.WriteLine("Eeeeenz VOOLEEVII non hai diritto al Bonus");
+                    Console.ReadLine();
+                    Console.WriteLine("Ma guarda che faccia!!Non se l'aspettava");
+                    Console.ReadLine();
+                    Console.WriteLine("Guardalo li con quelle manine che già aspettavano il bonus");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Non essendo maggiorenne non ti verrà dato un punteggio");
+            }
+            //interpolazione
+
         }
+
     }
 }
+
